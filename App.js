f9e484b77fd002904c3f3e7f8d7192a7d01275f1
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-11-05 19:11:11 -0300

    Criando a tela principal para encontrar o signo do usuário

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [nome, setNome] = useState("");
+  const [dataNascimento, setDataNascimento] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.logo}>Signo App</Text>
+      <View>
+        <Text style={styles.label}>Digite seu nome</Text>
+        <TextInput style={styles.input} keyboardType="default" value={nome} onChange={text => setNome(text)} placeholder="Nome" />
+        <Text style={styles.label}>Digite sua data de nascimento</Text>
+        <TextInput style={styles.input} keyboardType="default" value={dataNascimento} onChange={text => setDataNascimento(text)} placeholder="Data de Nascimento" />
+        <TouchableOpacity style={styles.btn}><Text style={{fontWeight: 'bold', fontSize: 16, textAlign: 'center'}}>Pesquisar o Signo</Text></TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -17,4 +26,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    fontSize: 16,
+    fontWeight: '700',
+    marginTop: 10,
+    color: "#000",
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: '800',
+    textAlign: 'left',
+    marginVertical: 10,
+    color: '#000',
+  },
+  input: {
+    borderRadius: 20,
+    borderStyle: 'solid',
+    color: '#000',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    backgroundColor: '#F0F0F0',
+  },
+  btn: {
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    color: "#000",
+    borderRadius: 20,
+    backgroundColor: '#D9D9D9',
+    marginTop: 18
+  }
 });
