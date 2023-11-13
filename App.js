2c72a55f6167106dc2d841f711fc504945aed458
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-11-13 14:30:07 -0300

    Instalando o Componente DateTimePicker e fazendo a  lógica dos signo, mas precisa ser melhorado

--- App.js
+++ App.js
@@ -1,22 +1,72 @@
 import { useState } from 'react'
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function App() {
   const [nome, setNome] = useState("");
-  const [dataNascimento, setDataNascimento] = useState("");
+  const [dataNascimento, setDataNascimento] = useState(new Date);
 
   return (
     <View style={styles.container}>
       <Text style={styles.logo}>Signo App</Text>
       <View>
         <Text style={styles.label}>Digite seu nome</Text>
-        <TextInput style={styles.input} keyboardType="default" value={nome} onChange={text => setNome(text)} placeholder="Nome" />
+        <TextInput
+          style={styles.input}
+          value={nome}
+          onChangeText={text => setNome(text)}
+          placeholder="Nome" />
         <Text style={styles.label}>Digite sua data de nascimento</Text>
-        <TextInput style={styles.input} keyboardType="default" value={dataNascimento} onChange={text => setDataNascimento(text)} placeholder="Ex: 19/01" />
-        <TouchableOpacity style={styles.btn}><Text style={{fontWeight: 'bold', fontSize: 16, textAlign: 'center'}}>Pesquisar o Signo</Text></TouchableOpacity>
+        <DateTimePicker
+          style={styles.input}
+          value={dataNascimento}
+          onChange={(event, selectedDate) => {
+            const currentDate = selectedDate || date;
+            setDataNascimento(currentDate);
+          }}
+          mode='date'
+          display="inline"
+        />
+        <TouchableOpacity style={styles.btn} onPress={getSign(dataNascimento)}><Text style={{ fontWeight: 'bold', fontSize: 16, textAlign: 'center' }}>Pesquisar o Signo</Text></TouchableOpacity>
+        {dataNascimento !== "" && (
+          <Text>Seu signo é de {getSign(dataNascimento)}</Text>
+        )}
       </View>
     </View>
   );
+}
+
+function getSign(date) {
+  const dia = date.getDate();
+  const mes = date.getMonth() + 1;
+
+  if ((mes == 1 && dia >= 20) || (mes == 2 && dia <= 18)) {
+    return "Aquário";
+  } else if ((mes == 2 && dia >= 19) || (mes == 3 && dia <= 20)) {
+    return "Peixes";
+  } else if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 19)) {
+    return "Áries";
+  } else if ((mes == 4 && dia >= 20) || (mes == 5 && dia <= 20)) {
+    return 'Touro';
+  } else if ((mes == 5 && dia >= 21) || (mes == 6 && dia <= 20)) {
+    return 'Gêmeos';
+  } else if ((mes == 6 && dia >= 21) || (mes == 7 && dia <= 22)) {
+    return 'Câncer';
+  } else if ((mes == 7 && dia >= 23) || (mes == 8 && dia <= 22)) {
+    return 'Leão';
+  } else if ((mes == 8 && dia >= 23) || (mes == 9 && dia <= 22)) {
+    return 'Virgem';
+  } else if ((mes == 9 && dia >= 23) || (mes == 10 && dia <= 22)) {
+    return 'Libra';
+  } else if ((mes == 10 && dia >= 23) || (mes == 11 && dia <= 21)) {
+    return 'Escorpião';
+  } else if ((mes == 11 && dia >= 22) || (mes == 12 && dia <= 21)) {
+    return 'Sagitário';
+  } else if ((mes == 12 && dia >= 22) || (mes == 1 && dia <= 19)) {
+    return 'Capricórnio';
+  } else {
+    return 'Desconhecido';
+  }
 }
 
 const styles = StyleSheet.create({
